705338bc1f37bd76c5ad1d6c9009653174d1d965
stelczyk <jakub.stelmach05@gmail.com>
2026-06-23 13:28:54 +0200

    init

---
+++ src/main/java/com/energymix/backend/EnergyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.energymix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/energymix/backend/EnergyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.energymix.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
